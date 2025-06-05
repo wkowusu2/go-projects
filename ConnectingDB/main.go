51c7fe66c5e479047367929a02e0d64704e4c2b4
william <tooknown27@gmail.com>
2025-06-05 23:17:17 +0000

    Added queries to the application

--- ConnectingDB/main.go
+++ ConnectingDB/main.go
@@ -4,12 +4,17 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq" // PostgreSQL driver
 )
 
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the Home Page!")
+	fmt.Println(r)
+}
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -33,4 +38,34 @@ func main() {
 
 	fmt.Println("✅ Successfully connected to database!")
 	defer db.Close()
+	//setting up the server
+	// mu := http.NewServeMux()
+	// server := &http.Server{
+	// 	Addr:    ":8080",
+	// 	Handler: mu,
+	// }
+	// mu.HandleFunc("/", handleHome)
+	// server.ListenAndServe()
+	// fmt.Println("Server is running on port 8080")
+	query := "SELECT * FROM test"
+	insertQuery := "INSERT INTO test (one) VALUES (1)"
+	_, err = db.Exec(insertQuery)
+	updateQuery := "UPDATE test SET one = 2 WHERE one = 1"
+	_, err = db.Exec(updateQuery)
+	deleteQuery := "DELETE FROM test WHERE one = 2"
+	_, err = db.Exec(deleteQuery)
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal("Error executing query:", err)
+	}
+	defer rows.Close()
+	fmt.Println("Query executed successfully!")
+	// fmt.Println("Rows returned:", rows)
+	for rows.Next() {
+		var one int
+		if err := rows.Scan(&one); err != nil {
+			log.Fatal("Error scanning row:", err)
+		}
+		fmt.Printf("ONE: %d\n", one)
+	}
 }
